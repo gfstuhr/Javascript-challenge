40e0b756fef274328487a7b86590266c746fdd46
Gabe Stuhr <66698826+gfstuhr@users.noreply.github.com>
2020-09-22 11:24:06 -0600

    Table Data added started filtering

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,20 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// Select Table
+var table = d3.select("tbody");
+
+//Add all Data to table
+tableData.forEach((ufo_sighting)=>{
+    var row = table.append("tr");
+    Object.entries(ufo_sighting).forEach(([key,value])=>{
+        var cell = row.append("td");
+        cell.text(value)
+    });
+});
+
+// Filtered Table
+var button = d3.select("#filter-btn")
+var form = d3.select("#datetime")
+
+console.log(form)
