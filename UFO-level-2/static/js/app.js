025e4429523176689dbc7234074a3ef60bda79eb
Gabe Stuhr <66698826+gfstuhr@users.noreply.github.com>
2020-09-24 14:15:52 -0600

    Revert "Updated State Filter"
    This reverts commit 793747ed4a08b23a32fc944559ad4f3bf0f346b7.

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -37,7 +37,7 @@ states = tableData.map(entry => entry.state);
 unique_states = states.filter(onlyUnique);
 var stateElement = d3.select("#State_Filter");
 blankoption=stateElement.append("option");
-blankoption.text("State");
+blankoption.text("None");
 unique_states.forEach((state)=>{
     var currentcity = stateElement.append("option");
     currentcity.text(state);
@@ -100,7 +100,7 @@ function runEnter(){
     if (cityValue!="City"){
     filteredData = filteredData.filter(city => city.city === cityValue);
     };
-    if (stateValue!="State"){
+    if (stateValue!="None"){
         filteredData = filteredData.filter(state => state.state === stateValue);
     };
     if (stateValue!="Country"){
